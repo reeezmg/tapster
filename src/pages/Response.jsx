dfa97daef98d1d19c30e64dd12595ac60221d3cb
reeezmg <64250221+reeezmg@users.noreply.github.com>
2025-03-04 11:07:11 +0530

    first commits

--- src/pages/Response.jsx
+++ src/pages/Response.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
-
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { ContactPreview } from './CardLanding/Types/ToContact';
+import { ProfilePreview } from './CardLanding/Types/ToDirectURL';
+import ShopLandingPage from './CardLanding/Types/ShopLanding';
+import PortfolioLanding from './CardLanding/Types/PortfolioLanding';
+import { EmbedLink } from './CardLanding/Types/ExternalLink';
 import testImage from "../Images/abstract1b.png"
 
 function Response() {
@@ -15,8 +21,7 @@ function Response() {
        alt="Local Test Image"
        className="w-full h-48 object-cover"
      />
-     jknkjn
-     
+     reez
     </div>
   );
 }
